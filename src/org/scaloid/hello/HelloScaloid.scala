e207dc02ed671292bc712bda568db7c49427c104
Andrea Brancaleoni <miwaxe@gmail.com>
2013-12-08 14:26:21 +0100

    Tether class

--- src/org/scaloid/hello/HelloScaloid.scala
+++ src/org/scaloid/hello/HelloScaloid.scala
@@ -2,22 +2,16 @@ package org.scaloid.hello
 
 import org.scaloid.common._
 import android.graphics.Color
+import com.stericson.RootTools.RootTools
+import com.stericson.RootTools.execution.CommandCapture
+import android.R
 
 class HelloScaloid extends SActivity {
 
   onCreate {
     contentView = new SVerticalLayout {
-      style {
-        case b: SButton => b.textColor(Color.RED).onClick(toast("Bang!"))
-        case t: STextView => t textSize 10.dip
-        case v => v.backgroundColor(Color.YELLOW)
-      }
-
-      STextView("I am 10 dip tall")
-      STextView("Me too")
-      STextView("I am taller than you") textSize 15.dip // overriding
-      SEditText("Yellow input field")
-      SButton(R.string.red)
+      SButton("Unlock Tether", Tether.unlock)
+      SButton("Lock Tether", Tether.lock)
     } padding 20.dip
   }
 
---
+++ src/org/scaloid/hello/Tether.scala
@@ -0,0 +1,42 @@
+package org.scaloid.hello
+
+import com.stericson.RootTools.RootTools
+import com.stericson.RootTools.execution.CommandCapture
+
+object Tether extends PIMP {
+  def unlock(): Boolean = {
+    lazy val shell = RootTools.getShell(true)
+
+    if (RootTools.isAccessGiven) {
+      try {
+        shell.add("iptables -t nat -A natctrl_nat_POSTROUTING -s 192.168.0.0/16 -o rmnet0 -j MASQUERADE")
+        shell.wait()
+      } catch {
+        case e: Exception => return false
+      }
+    }
+    true
+  }
+
+  def lock(): Boolean = {
+    lazy val shell = RootTools.getShell(true)
+
+    if (RootTools.isAccessGiven) {
+      try {
+        shell.add("iptables -t nat -D natctrl_nat_POSTROUTING 1")
+        shell.wait()
+      } catch {
+        case e: Exception => return false
+      }
+    }
+    true
+  }
+}
+
+class PIMP {
+  private var id = 0
+  implicit protected def stringToCommandCapture(string: String): CommandCapture = {
+    id += 1
+    new CommandCapture(id, string)
+  }
+}
